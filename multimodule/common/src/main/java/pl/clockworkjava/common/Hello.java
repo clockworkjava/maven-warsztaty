dd00b1ea0bc3876419ae6a93630a7034316c2b0c
Pawel Cwik <pawel@clockworkjava.pl>
2021-06-24 10:04:20 +0200

    Mutimodule

---
+++ multimodule/common/src/main/java/pl/clockworkjava/common/Hello.java
@@ -0,0 +1,8 @@
+package pl.clockworkjava.common;
+
+public class Hello {
+
+    public void hello() {
+        System.out.println("Hello World");
+    }
+}
---
+++ multimodule/desktop/src/main/java/pl/clockworkjava/desktop/App.java
@@ -0,0 +1,16 @@
+package pl.clockworkjava.desktop;
+
+import pl.clockworkjava.common.Hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Hello hello = new Hello();
+        System.out.println( "Hello World!" );
+    }
+}
